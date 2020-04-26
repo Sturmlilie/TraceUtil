88a1499b680013d95091beb1c1f82d982da8f41c
Sturmlilie <jonas.kulla@alumni.fh-aachen.de>
2020-04-27 00:13:14 +0200

    Annotater: Add simpler creation interface + whitespace fixes

--- src/main/java/net/ancurio/gldebug/trace/Annotater.java
+++ src/main/java/net/ancurio/gldebug/trace/Annotater.java
@@ -2,19 +2,23 @@ package net.ancurio.gldebug.trace;
 
 public interface Annotater {
 	void appendPrefix(String prefix);
-	
+
 	void push(String scope);
 	void pop();
-	
+
 	void insert(String message);
-	
+
 	void swap(String scope);
-	
+
 	enum ThreadAwareness {
 		IGNORANT,
 		AWARE
 	}
-	
+
+	static Annotater chooseImpl(String namespace) {
+		return chooseImpl(namespace, ThreadAwareness.IGNORANT);
+	}
+
 	static Annotater chooseImpl(String namespace, ThreadAwareness mode) {
 		if (GLAnnotater.isAvailable()) {
 			if (mode == ThreadAwareness.AWARE) {
