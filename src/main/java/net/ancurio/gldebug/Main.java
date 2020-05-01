e4703cf5fb9115ac789cacfed5ea6b99b567d00d
Sturmlilie <jonas.kulla@alumni.fh-aachen.de>
2020-05-01 13:17:43 +0200

    Rename class GLDebug -> Main

--- src/main/java/net/ancurio/gldebug/Main.java
+++ src/main/java/net/ancurio/gldebug/Main.java
@@ -5,7 +5,7 @@ import net.fabricmc.api.ModInitializer;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-public class GLDebug implements ModInitializer {
+public class Main implements ModInitializer {
 	public static final String MODID = "gldebug";
 	public static final Logger LOGGER = LogManager.getLogger(MODID);
 
