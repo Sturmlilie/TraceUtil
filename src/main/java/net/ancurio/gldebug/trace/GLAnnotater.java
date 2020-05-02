16a331e7c7ff561dcf2d2591decc15f50ca01004
Sturmlilie <jonas.kulla@alumni.fh-aachen.de>
2020-05-02 15:41:26 +0200

    Whitespace

--- src/main/java/net/ancurio/gldebug/trace/GLAnnotater.java
+++ src/main/java/net/ancurio/gldebug/trace/GLAnnotater.java
@@ -8,27 +8,27 @@ public class GLAnnotater extends AbstractAnnotater {
 	final static int ID = 0;
 	final static int TYPE = KHRDebug.GL_DEBUG_TYPE_MARKER;
 	final static int SEVERITY = KHRDebug.GL_DEBUG_SEVERITY_NOTIFICATION;
-	
+
 	public static boolean isAvailable() {
 		return GL.getCapabilities().GL_KHR_debug;
 	}
-	
+
 	public GLAnnotater(String namespace) {
 		super(namespace);
 	}
-	
+
 	@Override
 	public void push(String scope) {
 		super.push(scope);
 		KHRDebug.glPushDebugGroup(SOURCE, ID, prefix(scope));
 	}
-	
+
 	@Override
 	public void pop() {
 		super.pop();
 		KHRDebug.glPopDebugGroup();
 	}
-	
+
 	public void insert(String message) {
 		KHRDebug.glDebugMessageInsert(SOURCE, TYPE, ID, SEVERITY, prefix(message));
 	}
