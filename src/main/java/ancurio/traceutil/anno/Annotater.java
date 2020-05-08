7bba6088a4a32b899fed1b6986347855c7aa1e47
Sturmlilie <jonas.kulla@alumni.fh-aachen.de>
2020-05-08 04:50:47 +0200

    Annotater: Add javadoc comments

--- src/main/java/ancurio/traceutil/anno/Annotater.java
+++ src/main/java/ancurio/traceutil/anno/Annotater.java
@@ -1,13 +1,43 @@
 package ancurio.traceutil.anno;
 
 public interface Annotater {
+	/**
+	 * Optionally add a prefix to the annotations emitted by
+	 * this instance; the resulting annotations will look like this:
+	 * <namespace>:<prefix>/<object>
+	 *
+	 * @param prefix the prefix to append after the namespace
+	 */
 	void appendPrefix(String prefix);
 
+	/**
+	 * Descend one scope level deeper.
+	 * All GL calls / messages / scope after this call will be bundled
+	 * in this scope until the matching @pop is called.
+	 *
+	 * @param scope the name of the new nested scope
+	 */
 	void push(String scope);
+
+	/**
+	 * Close the currently active scope.
+	 */
 	void pop();
 
+	/**
+	 * Insert a freeform string message into the GL stream
+	 * at the current scope
+	 *
+	 * @param message a string to be inserted
+	 */
 	void insert(String message);
 
+	/**
+	 * Close the current active scope and open a new one.
+	 * Logically equivalent to @pop followed by @push.
+	 *
+	 * @param scope the name of the new scope
+	 */
 	void swap(String scope);
 
 	enum ThreadAwareness {
@@ -15,6 +45,15 @@ public interface Annotater {
 		AWARE
 	}
 
+	/**
+	 * Create an Annotater instance appropriate for the current
+	 * OpenGL context. A context _must_ be valid at call-time.
+	 * Will construct a dummy implementation when no sufficient
+	 * extension can be found.
+	 *
+	 * @param namespace the name of the message namespace
+	 * @return a valid implementation instance
+	 */
 	static Annotater chooseImpl(String namespace) {
 		return chooseImpl(namespace, ThreadAwareness.IGNORANT);
 	}
@@ -31,6 +70,13 @@ public interface Annotater {
 		}
 	}
 
+	/**
+	 * Query the name of the OpenGL extension that would be chosen
+	 * by an appropriate Annotater implementation when calling
+	 * @chooseImpl at this point.
+	 *
+	 * @return an OpenGL extenion string
+	 */
 	static String supportedImplExt() {
 		if (GLAnnotater.isAvailable()) {
 			return GLAnnotater.extensionString();
